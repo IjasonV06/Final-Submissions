f701440cb37bc42737c916cc5310a1d4d5cea465
IjasonV06 <161632918+IjasonV06@users.noreply.github.com>
2024-04-23 10:19:04 -0600

    Add files via upload

---
+++ practice turtle(blue triangle filled).py
@@ -0,0 +1,16 @@
+import turtle as t 
+
+turt1 = t.Turtle()
+#uncomment for triangle
+screen = t.Screen().setup(400,400)
+t.color('blue')
+t.begin_fill()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.width(5)
+t.forward(100)
+t.left(120)
+t.end_fill()
+
